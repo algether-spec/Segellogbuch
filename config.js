4fbc76563804caf81649ebb08e976360626d0fab
Alois Gether <al.gether@gmail.com>
2026-03-27 16:05:41 +0100

    v2.5.24: Notiz-Button in Log-Buch umbenannt

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    App-Version und Auto-Update-Logik
 ====================== */
 
-const APP_VERSION = "2.5.23";
+const APP_VERSION = "2.5.24";
 
 function updateButtonInit() {
     const btn = document.getElementById("btn-update");
